badb2c0ebb74f8968a4b5edf192136a636d193a6
Andrew Long <AndrewRLong02@gmail.com>
2021-02-16 19:01:03 -0700

    Optimizing .gitignore & deleting cached files

--- Sprite_Mover/Intermediate/Staging/Sprite_Mover.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-UE4_TRUE_SCRIPT_NAME=$(echo \"$0\" | xargs readlink -f)
-UE4_PROJECT_ROOT=$(dirname "$UE4_TRUE_SCRIPT_NAME")
-chmod +x "$UE4_PROJECT_ROOT/Engine/Binaries/Linux/UE4Game-Linux-Test"
-"$UE4_PROJECT_ROOT/Engine/Binaries/Linux/UE4Game-Linux-Test" \"../../../Sprite_Mover/Sprite_Mover.uproject\" "$@" 
